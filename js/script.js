8170d8979fd5bd5acafc411f49cd3a3a4402c1d8
Zvonimir <zvonimir.konjevic@gmail.com>
2022-12-14 22:40:15 +0100

    Create function showRoundWinner(), resetRoundWinner() and edit logic of playRound()
    Created fucntions showRoundWinner() that shows winner of every round in title div in HTML
    , function resetRoundWinner() that resets title to base value and adjusted logic
    of function playRound() so that it returns if round was win, lose or tie to determine
    what to show in title div.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const computerTitle = document.querySelector('#c-title');
 
 let humanScore = 0;
 let computerScore = 0;
+let roundResult = "";
 
 function getComputerChoice(){
     let index = Math.floor(Math.random() * AVAILABLECHOICES.length);
@@ -18,13 +19,34 @@ function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         humanScore += 0;
         computerScore += 0;
+        roundResult = "tie";
     }
     else if((playerSelection === 'Rock' && computerSelection === 'Scissors') || (playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Scissors' && computerSelection === 'Paper')){
         humanScore++;
+        roundResult = "win";
     }
     else{
         computerScore++;
+        roundResult = "lose";
     }
+}
+
+function showRoundWinner(roundResult){
+    if(roundResult === 'win'){
+        humanTitle.textContent = "Winner!";
+    }
+    else if(roundResult === 'lose'){
+        computerTitle.textContent = "Winner!";
+    }
+    else{
+        humanTitle.textContent = "Tie!";
+        computerTitle.textContent = "Tie!";
+    }
+}
+
+function resetRoundWinner(){
+    humanTitle.textContent = "Human";
+    computerTitle.textContent = "Computer";
 }
 
 function updateScore(){
@@ -47,6 +69,10 @@ for(let i=0; i<humanChoice.length; i++){
     humanChoice[i].addEventListener('click', () => {
         playRound(humanChoice[i].innerHTML, getComputerChoice());
         updateScore();
+        showRoundWinner(roundResult);
+        setTimeout(() => {
+            resetRoundWinner();
+        }, 700);
         setTimeout(() => {
             resetScore();
         }, 3000);
